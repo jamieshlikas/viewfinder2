5000b7e8599729726a2236bc032652fdb3fca5bf
Apple <apple@Apples-MacBook-Pro-16.local>
2019-06-25 10:50:05 -0400

    iteration 2 complete

--- viewFinder/addPhotoViewController.swift
+++ viewFinder/addPhotoViewController.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 class addPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
     @IBOutlet weak var imageView: UIImageView!
     
+    @IBOutlet weak var captionText: UITextField!
     var imagePicker = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +28,22 @@ class addPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
         imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true, completion: nil)
     }
+    @IBAction func imageSave(_ sender: Any) {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            let photoToSave = Photos(entity: Photos.entity(), insertInto: context)
+            
+            photoToSave.caption = captionText.text
+            
+            if let userImage = imageView.image {
+                if let userImageData = userImage.pngData() {
+                    photoToSave.imageData = userImageData
+                }
+            }
+            (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+              navigationController?.popViewController(animated: true)
+        }
+    }
+            
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
      
@@ -37,14 +53,5 @@ class addPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
         }
         imagePicker.dismiss(animated: true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
